33944c8ddfa2d112e4bee3ad697656a5d62a15db
Kevin Cruz <kevin_mcp@outlook.com>
2022-05-08 22:30:43 -0400

    Loading en el button de enviar

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { api } from "../../../lib/api";
 import { CloseButton } from "../../CloseButton";
+import { Loading } from "../../Loading";
 import { ScreenshotButton } from "../ScreenshotButton";
 
 type FeedbackContentStepProps = {
@@ -18,11 +19,14 @@ export function FeedbackContentStep({
 }: FeedbackContentStepProps) {
   const [screenshot, setScreenshot] = useState<string | null>(null);
   const [comment, setComment] = useState("");
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false);
 
   const feedbackTypeInfo = feedbackTypes[feedbackType];
 
   async function handleSubmitFeedback(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+
+    setIsSendingFeedback(true);
 
     // console.log({ screenshot, comment });
 
@@ -31,7 +35,7 @@ export function FeedbackContentStep({
       comment,
       screenshot,
     });
-
+    setIsSendingFeedback(false);
     onFeedbackSent();
   }
 
@@ -72,10 +76,10 @@ export function FeedbackContentStep({
 
           <button
             type="submit"
+            disabled={comment.length === 0 || isSendingFeedback}
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
-            disabled={comment.length === 0}
           >
-            Enviar Feedback
+            {!isSendingFeedback ? "Enviar Feedback" : <Loading />}
           </button>
         </footer>
       </form>
